31fe605ba0659ee5a76f9f0bab5e5d7b7e1e0a6e
gabecpf97 <gabrielcpf2997@gmail.com>
2023-10-16 21:42:36 -0700

    added basic file and ready to start on the front end

---
+++ src/component/RouteSwitch.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+
+const RouteSwitch = () => {
+  return (
+    <div className="container">
+      <BrowserRouter></BrowserRouter>
+    </div>
+  );
+};
+
+export default RouteSwitch;
